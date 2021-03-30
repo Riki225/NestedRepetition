6c9b783263e2bff36d26693c6dc23c0bc5db0481
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-03-30 14:38:59 +1100

    Nested Repetition

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NestedRepetition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string getname;
+            int counter;
+            int counter2;
+
+            counter = 0;
+
+            getname = Console.ReadLine();
+
+            while (counter < 10) {
+                counter2 = 0;
+
+                while (counter2 < 5) {
+                    Console.Write(getname);
+                    counter2 = counter2 + 1;
+                }
+
+                Console.WriteLine();
+                counter = counter + 1;
+            }
+
+            Console.WriteLine("Goodbye" + " " + getname);
+        }
+    }
+}
